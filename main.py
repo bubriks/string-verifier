26833e10eb44ee42f849b071c8b70a33afbeb07d
Ralf <bubriks@gmail.com>
2021-04-16 14:23:49 +0300

    Init

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import os
-import requests  # noqa We are just importing this to prove the dependency installed correctly
-
+import sys
+import re
 
 def main():
-    my_input = os.environ["INPUT_MYINPUT"]
-
-    my_output = f"Hello {my_input}"
-
-    print(f"::set-output name=myOutput::{my_output}")
-
-
+    expression = os.environ["INPUT_EXPRESSION"]
+    strings = os.environ["INPUT_STRINGS"]
+    
+    groups = None
+    for i in strings.split(" "):
+        m = re.match(expression, i)
+        if m:
+            if groups is None:
+                groups = m.groups
+            elif groups != m.groups:
+                sys.exit("Groups dont match" +
+                         "\nGroup 1: " + str(groups()) +
+                         "\nGroup 2: " + str(m.groups()))
+        else:
+            sys.exit("String doesnt match the pattern" +
+                     "\nString: " + i)
+    print(f"::set-output name=groups::{groups}")
+    
 if __name__ == "__main__":
     main()
